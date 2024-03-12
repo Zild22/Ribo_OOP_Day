7d081dd1cae9d5bc231aafed9cb5d448e045e1a0
Zild22 <xiltrix222@gmail.com>
2024-03-12 18:29:00 +0800

    OOP_ACTIVITIES

---
+++ RIBOLABACTIVITY1/src/ribolabactivity1/RiboLabActivity1.java
@@ -0,0 +1,34 @@
+
+ package ribolabactivity1;
+
+import java.util.Scanner;
+
+public class RiboLabActivity1 {
+
+     static Scanner input = new Scanner(System.in);
+     
+    public static void main(String[] args) {
+        System.out.print("Enter the message: ");
+        String msg = input.nextLine();
+        
+        String[] resultWords = msg.split("\\s+");
+        
+        boolean multiCap = false;
+        
+        for(String word: resultWords){
+            for(int i = 1; i < word.length(); i++){
+                if(Character.isUpperCase(word.charAt(i))){ 
+                    multiCap = true;
+                    break;
+                }
+            }
+        }
+        
+        if(multiCap){
+            System.out.println("JEJE!");
+        } else{
+            System.out.println("uWu");
+        }
+    }
+    
+}
